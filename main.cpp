cf5279c95c231e6701f19fb7f9b96999c10c7ea5
Marius Gžegoževskis <m.gzegozevskis@eif.viko.lt>
2021-10-10 21:06:10 +0300

    Basic list methods

--- main.cpp
+++ main.cpp
@@ -40,9 +40,48 @@ void insertFront(List* list, const string& value){
   temp->next = list->head;
   list->head = temp;
 }
-void insert();
+void insertByPos(List* list, int position, const string& value){
+  Node* previous = new Node;
+  Node* current = new Node;
+  Node* temp = new Node;
+  current = list->head;
+  for(int i=1; i < position; i++){
+    previous = current;
+    current = current->next;
+  }
+  temp->data = value;
+  previous->next = temp;
+  temp->next = current;
+}
 
-
+void deleteFront(List* list){
+  Node* temp = new Node;
+  temp = list->head;
+  list->head = list->head->next;
+  delete temp;
+}
+void deleteEnd(List* list){
+  Node* current = new Node;
+  Node* previous = new Node;
+  current = list->head;
+  while (current->next != nullptr){
+    previous = current;
+    current = current->next;
+  }
+  list->tail = previous;
+  previous->next = nullptr;
+  delete current;
+}
+void deleteByPos(List* list, int position){
+  Node* current = new Node;
+  Node* previous = new Node;
+  current = list->head;
+  for (int i = 1; i < position; i++) {
+    previous = current;
+    current = current->next;
+  }
+  previous->next = current->next;
+}
 
 int main() {
 
@@ -53,9 +92,14 @@ int main() {
   createNode(list, "Visogero");
   insertFront(list, "As esu pirmas Elementas");
   insertFront(list, "As esu pirmesnis Elementas");
-  displayList(list->head);
+  insertByPos(list, 2, "Pagal pozicija : 2");
 
-  delete list;
+  deleteByPos(list, 6);
+
+  deleteEnd(list);
+
+
+  displayList(list->head);
 
 
   return 0;
